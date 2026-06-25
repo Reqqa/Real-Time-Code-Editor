4555fb26cc71b95af431200b9c3498d5e1767fe1
Reqqa <tharmu34@gmail.com>
2026-06-26 00:38:59 +0800

    Test in ZED IDE

--- backend/target/debug/deps/backend.d
+++ backend/target/debug/deps/backend.d
@@ -1,6 +1,6 @@
-C:\Users\User\Documents\GitHub\Real-Time-Code-Editor\backend\target\debug\deps\backend.d: src\main.rs src\messages.rs src\room.rs src\ws_handler.rs
+C:\Users\User\Documents\Real-Time-Code-Editor\backend\target\debug\deps\backend.d: src\main.rs src\messages.rs src\room.rs src\ws_handler.rs
 
-C:\Users\User\Documents\GitHub\Real-Time-Code-Editor\backend\target\debug\deps\backend.exe: src\main.rs src\messages.rs src\room.rs src\ws_handler.rs
+C:\Users\User\Documents\Real-Time-Code-Editor\backend\target\debug\deps\backend.exe: src\main.rs src\messages.rs src\room.rs src\ws_handler.rs
 
 src\main.rs:
 src\messages.rs:
